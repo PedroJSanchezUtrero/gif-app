6e7d0510fcca78517f50fe5f686edebc1aebcae4
Pedro José Sánchez Utrero <pjutrero75@gmail.com>
2024-08-22 17:20:36 +0200

    feat: Refactor GifGatewayService to improve code readability and maintainability

--- src/app/gif-dashboard/services/gif-gateway.service.ts
+++ src/app/gif-dashboard/services/gif-gateway.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { ElementRef, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, Observable, of } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
 import { Gif } from '../interfaces/gif';
@@ -19,20 +19,11 @@ export class GifGatewayService {
   }
 
   public fetchGifs(query: string): void {
-    if (this.getLastQueryValue() === query || query === '' || this.getHasReachedMaxRequestsValue()) {
+    if (this.shouldSkipFetch(query)) {
       return;
     }
   
-    this.http.get<Gif>(`${environment.apiUrl}${query}`).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if (error.status === 429) {
-          this.hasReachedMaxRequests.next(true);
-        } else {
-          console.error('Error fetching GIFs', error);
-        }
-        return of(null);
-      })
-    ).subscribe((data) => {
+    this.makeHttpRequest(query).subscribe((data) => {
       if (data) {
         this.gifData.next(data);
         this.lastQuery.next(query);
@@ -66,5 +57,26 @@ export class GifGatewayService {
 
   private getHasReachedMaxRequestsValue(): boolean {
     return this.hasReachedMaxRequests.getValue();
+  }
+
+  private shouldSkipFetch(query: string): boolean {
+    return this.getLastQueryValue() === query || query === '' || this.getHasReachedMaxRequestsValue();
+  }
+
+  private makeHttpRequest(query: string): Observable<Gif | null> {
+    return this.http.get<Gif>(`${environment.apiUrl}${query}`).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.handleHttpError(error);
+        return of(null);
+      })
+    );
+  }
+
+  private handleHttpError(error: HttpErrorResponse): void {
+    if (error.status === 429) {
+      this.hasReachedMaxRequests.next(true);
+    } else {
+      console.error('Error fetching GIFs', error);
+    }
   }
 }
